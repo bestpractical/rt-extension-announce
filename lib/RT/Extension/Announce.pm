0aabd9145044d308c08d08a52e492cdff86a0bf0
Thomas Sibley <trs@bestpractical.com>
2012-10-04 13:03:13 -0700

    Bump version

--- lib/RT/Extension/Announce.pm
+++ lib/RT/Extension/Announce.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package RT::Extension::Announce;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 RT->AddJavaScript('announce.js');
 RT->AddStyleSheets('announce.css');
