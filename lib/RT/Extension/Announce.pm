96da4b8ee6a26e2a48a017311bf9b8df16fad3dd
sunnavy <sunnavy@bestpractical.com>
2019-10-01 00:07:54 +0800

    Update copyright year to 2019

--- lib/RT/Extension/Announce.pm
+++ lib/RT/Extension/Announce.pm
@@ -247,7 +247,7 @@ or via the web at
 
 =head1 LICENSE AND COPYRIGHT
 
-This software is Copyright (c) 2012-2014 by Best Practical Solutions, LLC
+This software is Copyright (c) 2012-2019 by Best Practical Solutions, LLC
 
 This is free software, licensed under:
 
