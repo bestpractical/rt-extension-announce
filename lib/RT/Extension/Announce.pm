8b62e73f8d6e3073dae2e6903e94590dbe2dd93b
sunnavy <sunnavy@bestpractical.com>
2020-07-20 21:35:44 +0800

    Prep 1.03

--- lib/RT/Extension/Announce.pm
+++ lib/RT/Extension/Announce.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package RT::Extension::Announce;
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 
 RT->AddJavaScript('announce.js');
 RT->AddStyleSheets('announce.css');
