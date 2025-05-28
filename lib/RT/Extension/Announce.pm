2470d9e99ae333ddbe69e098876e123c0b682588
Brad Embree <brad@bestpractical.com>
2025-05-28 14:26:41 -0700

    Prepare 1.06

--- lib/RT/Extension/Announce.pm
+++ lib/RT/Extension/Announce.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package RT::Extension::Announce;
 
-our $VERSION = '1.05';
+our $VERSION = '1.06';
 
 RT->AddJavaScript('announce.js');
 RT->AddStyleSheets('announce.css');
